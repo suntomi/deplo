3cca5705a4887cdb67698e854c5cd9cf8cdd9f50
Takehiro Iyatomi <iyatomi@gmail.com>
2020-05-07 13:26:26 +0900

    migrate to toml

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+### commands
 - ```deplo init```: init deplo project structure
     - mkdir config.common.data_dir
     - 
@@ -10,3 +11,79 @@
     - ```deplo infra plan```: generate plan
     - ```deplo infra apply```: apply generated plan
     - ```deplo infra eval```: evaluate value of infrastructure  
+
+### deplo datadir structure
+```
+- deplo
+    +- infra 
+        +- modules          # terraform modules
+    +- services             # service scripts
+    +- versions             # metadata json
+    +- resources            # data files like mobileprovision of iOS app
+```
+
+### deplo.toml example
+``` toml
+[common]
+deplo_image = "suntomi/deplo",
+data_dir = "meta"
+no_confirm_for_prod_deploy = false
+
+[cloud.provider.GCP]
+key = "$DEPLO_CLOUD_ACCESS_KEY"
+
+[cloud.terraformer.TerraformGCP]
+backend_bucket = "suntomi-publishing-generic-terraform"
+backend_bucket_prefix = "vault"
+root_domain = "suntomi.dev"
+project_id = "suntomi"
+region = "asia-northeast1"
+
+[vcs.Github]
+email = "suntomi.inc@gmail.com"
+account = "suntomi-bot"
+key = "$DEPLO_VCS_ACCESS_KEY"
+
+[ci.Circle]
+key = "$DEPLO_CI_ACCESS_KEY"
+    
+[client]
+org_name = "suntomi, inc."
+app_name = "dungeon of zoars"
+app_code = "doz"
+app_id = "dev.suntomi.app.doz"
+client_project_path = "./client"
+artifact_path = "/tmp/doz"
+version_config_path = "./meta/client/version"
+
+unity_path = "/Applications/Unity_2018.4.2f1/Unity.app/Contents/MacOS/Unity"
+serial_code = "$DEPLO_CLIENT_UNITY_SERIAL_CODE"
+account = "dokyogames@gmail.com"
+password": "$DEPLO_CLIENT_UNITY_ACCOUNT_PASSWORD"
+
+[[client.platform_build_configs.Android]]
+"keystore_password": "$DEPLO_CLIENT_ANDROID_KEYSTORE_PASSWORD",
+"keyalias_name": "doz",
+"keyalias_password": "$DEPLO_CLIENT_ANDROID_KEYALIAS_PASSWORD",
+"keystore_path": "./meta/client/Android/user.keystore",
+"use_expansion_file": false      
+
+[[client.platform_build_configs.IOS]]
+"team_id": "$DEPLO_CLIENT_IOS_TEAM_ID",
+"numeric_team_id": "$DEPLO_CLIENT_IOS_NUMERIC_TEAM_ID",
+"signing_password": "$DEPLO_CLIENT_IOS_P12_SIGNING_PASSWORD",
+"signing_plist_path": "./meta/client/iOS/suntomi_distribution.plist",
+"signing_p12_path": "./meta/client/iOS/suntomi_distribution.p12",
+"singing_provision_path": "./meta/client/iOS/suntomi_doz_appstore.mobileprovision" 
+
+[[client.stores.Apple]]
+account="suntomi.inc@gmail.com",
+password="$DEPLO_CLIENT_STORE_APPLE_PASSWORD"
+
+[[client.stores.Google]]
+key = "$DEPLO_CLIENT_STORE_GOOGLE_ACCESS_KEY"    
+```
+
+
+
+### service.toml example
--- src/config.rs
+++ src/config.rs
@@ -1,12 +1,15 @@
 use std::fs;
 
+use log;
 use simple_logger;
 use serde::{Deserialize, Serialize};
-use serde_json::Result;
+use toml::de::{Error};
+use dotenv::dotenv;
 
 use crate::args;
 
 #[derive(Serialize, Deserialize)]
+#[serde(tag = "type")]
 pub enum CloudProviderConfig {
     GCP {
         key: String
@@ -20,6 +23,7 @@ pub enum CloudProviderConfig {
     }
 }
 #[derive(Serialize, Deserialize)]
+#[serde(tag = "type")]
 pub enum CIConfig {
     Github {
         account: String,
@@ -30,6 +34,7 @@ pub enum CIConfig {
     }
 }
 #[derive(Serialize, Deserialize)]
+#[serde(tag = "type")]
 pub enum StoreConfig {
     Apple {
         account: String,
@@ -40,6 +45,7 @@ pub enum StoreConfig {
     }
 }
 #[derive(Serialize, Deserialize)]
+#[serde(tag = "type")]
 pub enum VCSConfig {
     Github {
         email: String,
@@ -53,6 +59,7 @@ pub enum VCSConfig {
     }
 }
 #[derive(Serialize, Deserialize)]
+#[serde(tag = "type")]
 pub enum TerraformerConfig {
     TerraformGCP {
         backend_bucket: String,
@@ -68,6 +75,7 @@ pub struct CloudConfig {
     pub terraformer: TerraformerConfig,
 }
 #[derive(Serialize, Deserialize)]
+#[serde(tag = "type")]
 pub enum PlatformBuildConfig {
     Android {
         keystore_password: String,
@@ -86,7 +94,7 @@ pub enum PlatformBuildConfig {
     }
 }
 #[derive(Serialize, Deserialize)]
-pub struct ClientBuildConfig {
+pub struct ClientConfig {
     pub org_name: String,
     pub app_name: String,
     pub app_code: String,
@@ -124,35 +132,53 @@ pub struct Config<'a> {
     pub cloud: CloudConfig,
     pub vcs: VCSConfig,
     pub ci: CIConfig,
-    pub client_build: ClientBuildConfig,
+    pub client: ClientConfig,
 }
 
 impl<'a> Config<'a> {
     // static factory methods 
-    pub fn load(path: &str) -> Result<Config> {
+    pub fn load(path: &str) -> Result<Config, Error> {
         let mut content = fs::read_to_string(path).unwrap();
         if envsubst::is_templated(&content) {
             content = envsubst::substitute(&content, &std::env::vars().collect()).unwrap();
         }
-        return serde_json::from_str(&content);
+        return toml::from_str(&content);
     }
-    pub fn create<A: args::Args>(args: &'a A) -> Result<Config> {
-        let mut c = Config::load(args.value_of("config").unwrap_or("./deplo.json")).unwrap();
+    pub fn create<A: args::Args>(args: &'a A) -> Result<Config, Error> {
+        let verbosity = args.occurence_of("verbosity");
+        simple_logger::init_with_level(match verbosity {
+            0 => log::Level::Warn,
+            1 => log::Level::Info,
+            2 => log::Level::Debug,
+            3 => log::Level::Trace,
+            _ => log::Level::Warn
+        }).unwrap();
+        // load dotenv
+        match args.value_of("dotenv") {
+            Some(dotenv_path) => {
+                dotenv::from_filename(dotenv_path).unwrap();
+            },
+            None => match dotenv() {
+                Ok(_) => {},
+                Err(err) => {
+                    log::warn!(".env not present or cannot load by error [{:?}], this usually means:\n\
+                                1. command will be run with incorrect parameter or\n\
+                                2. secrets are directly written in deplo.toml\n\
+                                please use .env to store secrets, or use -e flag to specify its path", 
+                                err)
+                } 
+            },
+        };
+        //println!("DEPLO_CLIENT_IOS_TEAM_ID:{}", std::env::var("DEPLO_CLIENT_IOS_TEAM_ID").unwrap());    
+        let mut c = Config::load(args.value_of("config").unwrap_or("./deplo.toml")).unwrap();
         c.cli = CliConfig {
-            verbosity: args.occurence_of("verbosity"),
+            verbosity,
             dryrun: args.occurence_of("dryrun") > 0,
             debug: match args.values_of("debug") {
                 Some(s) => s,
                 None => vec!{}
             }
         };
-        simple_logger::init_with_level(match c.cli.verbosity {
-            0 => log::Level::Error,
-            1 => log::Level::Info,
-            2 => log::Level::Debug,
-            3 => log::Level::Trace,
-            _ => log::Level::Info
-        }).unwrap();
         return Ok(c);
     }
 }
